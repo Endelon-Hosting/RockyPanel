47f80960ade401baa36de1e2b18562df89fd657c
Dalk-GitHub <67603460+Dalk-GitHub@users.noreply.github.com>
2021-11-08 16:37:47 +0100

    Revert "Merge branch 'master' of https://github.com/Endelon-Hosting/RockyPanel"
    This reverts commit 073dc740e87bcd6a01e6183207535eb4d91a90af, reversing
    changes made to 55f0af9ffabfdb05dd5808a0d3b576be3cae0e75.

--- RockyPanelBackend/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RockyPanelBackend
-{
-    public class Constants
-    {
-        public static string Version { get; set; } = "Canary";
-        public static string PanelName { get; set; } = "Rocky";
-        public static string Theme { get; set; } = "dashtreme";
-    }
-}

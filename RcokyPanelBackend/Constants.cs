274c05398aa806254eff7d068069a9c757f8ee48
Marcel Baumgartner <marcel.kbkm@gmail.com>
2021-11-08 16:10:35 +0100

    Added theme

---
+++ RcokyPanelBackend/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace RcokyPanelBackend
+{
+    public class Constants
+    {
+        public string Version { get; set; } = "v1.0";
+        public string PanelName { get; set; } = "Rocky (Canary)";
+    }
+}
